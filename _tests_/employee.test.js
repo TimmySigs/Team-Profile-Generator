ebdfb1bc50147e2abdab28c3935b28b3488dff78
TimmySigs <108436098+TimmySigs@users.noreply.github.com>
2022-09-29 02:09:44 -0500

    "started testing"

---
+++ _tests_/employee.test.js
@@ -0,0 +1,26 @@
+const Employee = require("../lib/employee.js");
+
+describe("Getters", () => {
+    const name = "Peter Gibbons";
+    const id = 33;
+    const email = "peter.gibbons@initech.com";
+    const role = "Employee";
+
+    const testEmployee = new Employee(name, id, email);
+
+    it("Should return the Employee name when requested", () => {
+        expect(testEmployee.getName()).toBe(name);
+    });
+
+    it("Should return the Employee id when requested", () => {
+        expect(testEmployee.getId()).toBe(id);
+    });
+
+    it("Should return the Employee email when requested", () => {
+        expect(testEmployee.getEmail()).toBe(email);
+    });
+
+    it("Should return the Employee role when requested", () => {
+        expect(testEmployee.getRole()).toBe(role);
+    });
+});
---
+++ _tests_/engineer.test.js
@@ -0,0 +1,32 @@
+const Engineer = require("../lib/Engineer.js");
+
+describe("Engineer", () => {
+    const name = "Samir Nagheenanaj";
+    const id = 12;
+    const email = "samir.nagheenanaj@initech.com";
+    const gitHubUsername = "bravotango";
+    const role = "Engineer";
+    const returnedGitHub = `<a href="https://github.com/${gitHubUsername}">https://github.com/${gitHubUsername}</a>`;
+
+    const testEngineer = new Engineer(name, id, email, gitHubUsername);
+
+    it("Should return github linked account", () => {
+        expect(testEngineer.getGithub()).toBe(returnedGitHub);
+    });
+
+    it("Should return the Engineer name when requested", () => {
+        expect(testEngineer.getName()).toBe(name);
+    });
+
+    it("Should return the Engineer id when requested", () => {
+        expect(testEngineer.getId()).toBe(id);
+    });
+
+    it("Should return the Engineer email when requested", () => {
+        expect(testEngineer.getEmail()).toBe(email);
+    });
+
+    it("Should return the Engineer role when requested", () => {
+        expect(testEngineer.getRole()).toBe(role);
+    });
+});
